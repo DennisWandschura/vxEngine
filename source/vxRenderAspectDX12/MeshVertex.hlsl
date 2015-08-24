540193a6d3195f959618f87bc402f7da440ba04d
DennisWandschura <dennis.wandschura@gmx.de>
2015-08-24 14:14:50 +0200

    removed file

--- source/vxRenderAspectDX12/MeshVertex.hlsl
+++ source/vxRenderAspectDX12/MeshVertex.hlsl
@@ -14,11 +14,7 @@ struct Vertex
 struct GSInput
 {
 	float3 wsPosition : POSITION0;
-	//float4 positionPrev : POSITION1;
-	//float3 vsPosition : POSITION2;
 	float3 vsNormal : NORMAL0;
-	//float3 vsTangent : TANGENT0;
-	//float3 vsBitangent : BITANGENT0;
 	float2 texCoords : TEXCOORD0;
 	uint material : BLENDINDICES0;
 };
@@ -30,16 +26,6 @@ cbuffer CameraBuffer : register(b0)
 
 StructuredBuffer<TransformGpu> s_transforms : register(t0);
 StructuredBuffer<uint> s_materials : register(t1);
-
-/*float3 getPreviousWsPosition(in float3 position, uint index)
-{
-	float3 translation = s_transformsPrev[index].translation.xyz;
-	float4 qRotation = unpackQRotation(s_transformsPrev[index].packedQRotation);
-
-	float3 wsPosition = quaternionRotation(position, qRotation) + translation;
-
-	return wsPosition;
-}*/
 
 GSInput main(Vertex input)
 {
@@ -58,20 +44,10 @@ GSInput main(Vertex input)
 
 	float3x3 normalMatrix = (float3x3)cameraBuffer.viewMatrix;
 	float3 vsNormal = mul(normalMatrix, wsNormal);
-	float3 vsTangent = mul(normalMatrix, wsTangent);
-	float3 vsBitangent = mul(normalMatrix, wsBitangent);
-
-	//float3 wsPositionPrev = getPreviousWsPosition(input.position, elementId);
-	//s_transformsPrev[elementId] = s_transforms[elementId];
 
 	GSInput output;
 	output.wsPosition = wsPosition;
-	//output.position = mul(cameraBuffer.pvMatrix, float4(wsPosition, 1));
-	//output.vsPosition = mul(cameraBuffer.viewMatrix, float4(wsPosition, 1)).xyz;
-	//output.positionPrev = output.position;//mul(cameraBuffer.pvMatrixPrev, float4(wsPositionPrev, 1));
 	output.vsNormal = vsNormal;
-	//output.vsTangent = vsTangent;
-	//output.vsBitangent = vsBitangent;
 	output.texCoords = input.texCoords;
 	output.material = s_materials[materialIndex];
 
